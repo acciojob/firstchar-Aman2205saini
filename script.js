e956553dfc73752cd9f017b5f8c8fcf294a0b329
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2024-11-26 22:01:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function firstChar(text) {
   for(var i=0;i<=text.length;i++){
 	  if(text.charat(i)!=" "){
-		  return text.charAt(i);
+		  alert( text.charAt(i));
 	  }
   }
-	return"";
+	"";
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
